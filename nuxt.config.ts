047ccfd07e5a56dcfdab76ac23a9afb1022a4b1d
Jordach <thejordach@gmail.com>
2025-07-17 21:09:38 -0700

    migration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,11 +8,22 @@ export default defineNuxtConfig({
     plugins: [
       tailwindcss(),
     ],
+    resolve: {
+      alias: {
+        '.prisma/client/index-browser': './node_modules/.prisma/client/index-browser.js',
+      },
+    },
   },
   supabase:{
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
     redirect:false,
   },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxtjs/supabase']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxtjs/supabase',
+    '@prisma/nuxt'
+  ],
+
 })
