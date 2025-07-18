fa5729abe523a084410ae2257e3b7db8a08656fa
Jordach <thejordach@gmail.com>
2025-07-17 22:10:32 -0700

    seed database

---
+++ prisma/seed.ts
@@ -0,0 +1,29 @@
+import {PrismaClient} from '@prisma/client'
+import {$fetch} from 'ofetch'
+
+const prisma = new PrismaClient()
+
+const productsURL = 'https://fakestoreapi.com/products'
+const products = await $fetch(productsURL)
+
+const seedProducts = async () => {
+    try {
+        for (const product of products) {
+            await prisma.products.create({
+                data:{
+                    title: product.title,
+                    description: product.description,
+                    image:product.image,
+                    category: product.category,
+                    price: product.price
+                }
+            })
+        }
+    } catch (error) {
+        console.error(error)
+    } finally {
+        await prisma.$disconnect()
+    }
+}
+
+await seedProducts()
